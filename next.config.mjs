84becf4ac7080f12f8ed28f277719e6d41505587
yamamara <yamamrtyu@gmail.com>
2026-07-31 08:47:49 -0400

    Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: '/Viskar',
 }
 
 export default nextConfig
